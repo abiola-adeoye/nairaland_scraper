4ec3e5beeaf24a803032119e6f494453bde324ad
abiola-adeoye <slmadeoye@gmail.com>
2023-01-03 14:10:02 +0100

    refactored code: all extracts work

--- nairaland.py
+++ nairaland.py
@@ -1,7 +1,7 @@
 """sometimes there are empty table row tags which have no data, in such situations len(tr) doesn't exceed 1 item
 with find all, so we skip the rest of code if less than or equal to 1 item"""
 import requests
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional, Any
 from itertools import cycle
 from bs4 import BeautifulSoup
 
@@ -16,7 +16,8 @@ class NairalandSoup:
         self.current_page = self.start_page
         self.last_page = 20
 
-    def _get_response(self):
+    def _get_response(self) -> Union[str, bytes]:
+        """ Gets the response content of page we want to scrape"""
         try:
             response = requests.get(self.url_structure + str(self.current_page))
             return response.content
@@ -39,14 +40,20 @@ class NairalandScrapper(NairalandSoup):
 
     def __init__(self, search_word):
         super().__init__(search_word)
+        super().make_soup_object()
+        super()._get_num_search_results()
         self.headline_mod_cycle = cycle([0, 1])
         self.headline_mod_num = next(self.headline_mod_cycle)
 
         self.post_mod_cycle = cycle([1, 0])
         self.post_mod_num = next(self.post_mod_cycle)
 
+    def scrape_nairaland(self):
+        for page in range(int(self.last_page)):
+            post = self.get_post()
+
     # extracts table row tags on result page (headline and actual post)
-    def get_page_tr(self):
+    def _get_page_tr(self) -> List[str]:
         nairaland_stats_for_day = self.soup.table   # I want to extract data here to use
         post_block = nairaland_stats_for_day.find_next_sibling("table")
         page_tr = post_block.find_all("tr")
@@ -71,7 +78,7 @@ class NairalandScrapper(NairalandSoup):
         if len(tr_tag_value) < 1:      # changed from <= 1 to <
             return True
 
-    def _get_headline_details(self, post_headline):
+    def _get_headline_details(self, post_headline: str) -> Optional[dict[str, Union[str, Any]]]:
         headline_details = {}
         headline_values = post_headline.find_all("a")
         if self._check_tr_tag_empty(headline_values):  # if True post table row tag is empty, i.e. no post
@@ -89,7 +96,7 @@ class NairalandScrapper(NairalandSoup):
         return headline_details
 
     # post headline start from 1st tr tag
-    def get_headlines(self, page_tr: str) -> List[Dict[str, Union[str]]]:
+    def get_headlines(self, page_tr: List) -> List[Dict[str, Union[str]]]:
         headlines = []
 
         for headline_id in range(len(page_tr)):
@@ -101,55 +108,54 @@ class NairalandScrapper(NairalandSoup):
         return headlines
 
     @staticmethod
-    def extract_post_block(post_table_tr_data):
+    def _extract_post_block(post_table_tr_data):
         return post_table_tr_data.find('div')
 
     @staticmethod
-    def extract_blockquote(post_block_data):
+    def _extract_blockquote(post_block_data):
         """ Extracts the tag where a previous post is quoted, returns the tag or None"""
         return post_block_data.find('blockquote')
 
     @staticmethod
-    def extract_quote_status(blockquote_data):
+    def _extract_quote_status(blockquote_data) -> bool:
         """Returns True if there was a quote"""
         if blockquote_data:
             return True
         return False
 
     @staticmethod
-    def extract_quote_text(blockquote_data):
+    def _extract_quote_text(blockquote_data) -> str:
         if blockquote_data:
             quoted_post = blockquote_data.extract().text
             return quoted_post
         return blockquote_data
 
     @staticmethod
-    def extract_post_statistic(post):
+    def _extract_post_statistic(post: str) -> str:
         post_statistics = post.find('p')  # likes and shares
         if post_statistics:
             return post_statistics.text
         return ""
 
-    def _get_post_details(self, post_content):
+    def _get_post_details(self, post_content: str) -> Dict[str, str]:
         post_details = {}
 
-        post_block = self.extract_post_block(post_content)
+        post_block = self._extract_post_block(post_content)
 
-        blockquote = self.extract_blockquote(post_block)
-        post_details['quote'] = self.extract_quote_status(blockquote)
-        post_details['quoted_post'] = self.extract_quote_text(blockquote)
+        blockquote = self._extract_blockquote(post_block)
+        post_details['quote'] = self._extract_quote_status(blockquote)
+        post_details['quoted_post'] = self._extract_quote_text(blockquote)
         post_details['post_text'] = post_block.text
-        post_details['statistics'] = self.extract_post_statistic(post_content)
+        post_details['statistics'] = self._extract_post_statistic(post_content)
 
         return post_details
 
-    def get_post(self, page_tr):
+    def get_post(self, page_tr: List) -> List[Dict[str, str]]:
         post = []
 
         for post_id in range(len(page_tr)):
             if post_id % 2 == self.post_mod_num:
                 details = self._get_post_details(post_content=page_tr[post_id])
                 post.append(details)
-                print(details)
-
         return post
+
